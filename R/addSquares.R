07503e2b46fea1be4851c6f5b2f55ff9e094cfb1
Necla Kochan <necla.kayaalp@yahoo.com>
2023-08-09 14:35:33 +0300

    Create addSquares.R
    function updated

---
+++ R/addSquares.R
@@ -0,0 +1,36 @@
+# TODO: Add comment
+#
+# Author: Necla Kochan
+###############################################################################
+
+#' @title Sum of squares
+#'
+#' @description The \code{addSquares} function takes the sum of squares of two
+#' variables
+#'
+#' @param x a \code{numeric} vector in size n
+#'
+#' @param y a \code{numeric} vector in size n
+#'
+#' @return a vector of type \code{numeric} in size n
+#'
+#' @author Necla Kochan
+#'
+#' @examples
+#' # call data
+#' data(iris)
+#'
+#' # define the function paramaters
+#' x <- iris$Sepal.Length
+#' y <- iris$Petal.Length
+#'
+#' # execute the function
+#' addSquares(x = x, y = y)
+#'
+#' @export
+
+addSquares <- function(x, y){
+
+  return(list(square = (x^2 - y^2), x = x, y = y))
+
+}
